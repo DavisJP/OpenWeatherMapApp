43768aa933c799277bf7529c534db12c11f946c0
Davis Miyashiro <davis.miyashiro@gmail.com>
2026-04-27 12:45:06 +0100

    Upgrade dependencies.

--- app/src/main/java/com/davismiyashiro/weathermapapp/presentation/ForecastListActivity.kt
+++ app/src/main/java/com/davismiyashiro/weathermapapp/presentation/ForecastListActivity.kt
@@ -27,7 +27,7 @@ package com.davismiyashiro.weathermapapp.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
